e724cc79534c7fef63994d65a2b077b9f8076770
Abbash Ali <abbasali6021@gmail.com>
2023-12-26 21:09:15 +0530

    connected client and server webSockets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from 'react'
 import './App.css'
 import MainGrid from './components/MainGrid'
 
 function App() {
+  let clientId
+
+  useEffect(() => {
+    let ws =  new WebSocket("ws://localhost:9090")
+
+    ws.onmessage = message => {
+      // -since we are sending stringified data from server
+      const response = JSON.parse(message.data)
+
+      if(response.method === 'connect') {
+        clientId = response.clientId
+        console.log('client id set successfully - ' + clientId)
+      }
+    }
+
+    ws.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+  }, [])
 
   return (
     <div className='w-screen h-screen'>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,43 @@
+const crypto = require('crypto');
+const webSocketServer = require('websocket').server
 const http = require('http')
 
-const server = http.createServer((req,res) => {
-    if(req.url === '/') {
-        res.end('Hello there!!')
-    }
+// no callbacks, because it will anyway be overwritten by the socket connection
+const httpServer = http.createServer()
+httpServer.listen(9090, () => {
+    console.log("Server is listening")
 })
 
+let clients = {}
+const wsServer = new webSocketServer({
+    "httpServer": httpServer
+})
 
-server.listen(4000, () => {
-    console.log("Server is listening")
+// -Event listener for WebSocket connection requests
+wsServer.on("request", request => {
+    //accepting connection
+    const connection = request.accept(null, request.origin)
+
+    //open and close event
+    connection.on("open", () => console.log('Opened!'))
+    connection.on("close", (event) => console.log('Closed! ', event))
+
+    // incoming message event
+    connection.on("message", message => {
+        const result = JSON.parse(message.utf8Data)
+        console.log(result)
+    })
+
+    const clientId = crypto.randomUUID()
+    clients[clientId] = {
+        "connection": connection
+    }
+
+    const payload = {
+        "method": "connect",
+        "clientId": clientId
+    }
+
+    // sending back to the client
+    connection.send(JSON.stringify(payload))
 })
