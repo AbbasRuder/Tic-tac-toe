2d39eecd9d7458a489707aba59c129851f364019
Abbash Ali <abbasali6021@gmail.com>
2023-12-28 23:26:59 +0530

    add create and join a game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,130 @@
-import { useEffect } from 'react'
-import './App.css'
-import MainGrid from './components/MainGrid'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import MainGrid from "./components/MainGrid";
 
 function App() {
-  let clientId
+  const [showCreateGame, setShowCreateGame] = useState(true);
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  // let clientId: string;
+  let gameId: string | null = null;
+
+  const clientIdRef = useRef<string | null>(null);
+  const wsRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    let ws =  new WebSocket("ws://localhost:9090")
+    wsRef.current = new WebSocket("ws://localhost:9090");
 
-    ws.onmessage = message => {
-      // -since we are sending stringified data from server
-      const response = JSON.parse(message.data)
+    wsRef.current.onmessage = (message) => {
+      // -since we are receiving stringified data from server
+      const response = JSON.parse(message.data);
 
-      if(response.method === 'connect') {
-        clientId = response.clientId
-        console.log('client id set successfully - ' + clientId)
+      //first response from the server
+      if (response.method === "connect") {
+        clientIdRef.current = response.clientId;
+        console.log("client id set successfully - " + clientIdRef.current);
       }
+
+      //receiving response after creating a game
+      if (response.method === "create") {
+        const { game } = response;
+        gameId = game.gameId;
+        console.log("game created with game state - ", game);
+      }
+
+      //receiving response after joining a game
+      if (response.method === "join") {
+        const { game } = response;
+        console.log('joined game with game obj - ', game)
+      }
+    };
+
+    wsRef.current.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+  }, []);
+
+  const handleCreateGame = () => {
+    const payload = {
+      method: "create",
+      clientId: clientIdRef.current,
+    };
+    wsRef.current?.send(JSON.stringify(payload));
+    setShowCreateGame(false);
+  };
+
+  const handleJoinGame = () => {
+    if (inputValue.trim() === "") {
+      alert("Enter the game id");
+      return;
     }
 
-    ws.onerror = (error) => {
-      console.error('WebSocket error:', error);
+    if (!gameId) {
+      gameId = inputValue;
+    }
+    console.log("client", clientIdRef.current);
+    const payload = {
+      method: "join",
+      clientId: clientIdRef.current,
+      gameId: gameId,
     };
-  }, [])
+
+    // console.log('readystate? ',wsRef.current?.readyState);
+    // if (wsRef.current?.readyState === WebSocket.OPEN) {
+    //   wsRef.current?.send(JSON.stringify(payload));
+    // } else {
+    //   console.error("WebSocket is not open");
+    // }
+
+    wsRef.current?.send(JSON.stringify(payload));
+    setShowCreateGame(false);
+    setShowInput(false);
+  };
 
   return (
-    <div className='w-screen h-screen'>
-      <p className='text-center text-xl font-bold'>Tic tac toe</p>
+    <div className="w-screen h-screen">
+      {/* <p className='text-center text-xl font-bold'>Tic tac toe</p> */}
       <MainGrid />
+
+      {showCreateGame && (
+        <div className="fixed inset-x-0 inset-y-0 bg-slate-300/80 flex flex-col gap-2 justify-center items-center">
+          <button
+            className="bg-blue-400 w-36 py-2 px-3 rounded text-xl text-white font-semibold"
+            onClick={handleCreateGame}
+          >
+            Create Game
+          </button>
+          <button
+            className="bg-blue-400 w-36 py-2 px-3 rounded text-xl text-white font-semibold"
+            onClick={() => setShowInput(true)}
+          >
+            Join Game
+          </button>
+        </div>
+      )}
+
+      {showInput && (
+        <div className="fixed inset-x-0 inset-y-0 bg-slate-300/80 flex flex-col gap-2 justify-center items-center">
+          <div className="flex flex-col">
+            <label htmlFor="gameId ">Enter Game ID</label>
+            <input
+              id="gameId"
+              className="p-1 border-2 border-black/50 rounded"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button
+              className="mt-2 bg-blue-400 py-1 px-4 rounded text-lg text-white font-semibold"
+              onClick={handleJoinGame}
+            >
+              Join
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -9,35 +9,87 @@ httpServer.listen(9090, () => {
 })
 
 let clients = {}
+let games = {}
+
 const wsServer = new webSocketServer({
     "httpServer": httpServer
 })
 
 // -Event listener for WebSocket connection requests
 wsServer.on("request", request => {
-    //accepting connection
+    //-accepting connection
     const connection = request.accept(null, request.origin)
 
     //open and close event
     connection.on("open", () => console.log('Opened!'))
     connection.on("close", (event) => console.log('Closed! ', event))
 
-    // incoming message event
+    // incoming message events
     connection.on("message", message => {
         const result = JSON.parse(message.utf8Data)
-        console.log(result)
+
+        //method for creating a game
+        if (result.method === "create") {
+            const clientId = result.clientId
+            const gameId = crypto.randomUUID()
+            games[gameId] = {
+                "gameId": gameId,
+                "gridCells": 9,
+                "clients": []
+            }
+
+            games[gameId].clients.push({
+                "clientId": clientId,
+                "mark": "X"
+            })
+
+            const payload = {
+                "method": "create",
+                "game": games[gameId]
+            }
+            console.log('game is created')
+            const con = clients[clientId].connection
+            con.send(JSON.stringify(payload))
+        }
+
+        //method for joining a game
+        if (result.method === "join") {
+            const { clientId, gameId } = result
+            const game = games[gameId]
+
+            const clientLength = game.clients.length
+            if(clientLength > 2) {
+                console.log("max player reached")
+                return
+            }
+            // const mark = {"0": "X", "1": "O"}[clientLength]
+            game.clients.push({
+                "clientId": clientId,
+                "mark": "O"
+            })
+
+
+            const payload = {
+                "method": "join",
+                game: game
+            }
+
+            //sending all the clients a payload
+            game.clients.forEach(obj => {
+                clients[obj.clientId].connection.send(JSON.stringify(payload))
+            });
+        }
     })
 
     const clientId = crypto.randomUUID()
+    //-binding the clientId with the socket connection in the clients object
     clients[clientId] = {
         "connection": connection
     }
-
     const payload = {
         "method": "connect",
         "clientId": clientId
     }
-
-    // sending back to the client
+    // -first payload to the client
     connection.send(JSON.stringify(payload))
 })
